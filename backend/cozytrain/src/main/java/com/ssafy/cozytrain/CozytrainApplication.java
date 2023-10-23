857c0b98adecb2b20c26a15eda4312b8796c3a4a
황주원 <woneee99@gmail.com>
2023-10-23 17:49:28 +0900

    Feat: file structure

---
+++ backend/cozytrain/src/main/java/com/ssafy/cozytrain/CozytrainApplication.java
@@ -0,0 +1,13 @@
+package com.ssafy.cozytrain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CozytrainApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CozytrainApplication.class, args);
+    }
+
+}
