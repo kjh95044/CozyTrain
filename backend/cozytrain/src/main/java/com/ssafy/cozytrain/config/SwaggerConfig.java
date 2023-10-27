d87d5252979e252abdee2de3b399281eefd67a44
KimJiHong <kjh95044@naver.com>
2023-10-27 10:43:47 +0900

    Chore: Test Swagger config

--- backend/cozytrain/src/main/java/com/ssafy/cozytrain/config/SwaggerConfig.java
+++ backend/cozytrain/src/main/java/com/ssafy/cozytrain/config/SwaggerConfig.java
@@ -6,16 +6,19 @@ import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Server;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
+import java.util.Collections;
+
 @Configuration
 public class SwaggerConfig {
-
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.OAS_30)
                 .apiInfo(apiInfo())
+                .servers(new Server("root", "/", "root", Collections.emptyList(), Collections.emptyList()))
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ssafy.cozytrain"))
                 .paths(PathSelectors.any())
