4780f99cded1f1e8fecee8ef6fd387282e25d924
이하린 <gmlwls608@gmail.com>
2023-10-30 17:42:01 +0900

    Feat: 지구 3D 모델 띄우기
    Change-Id: Icaf655d26050778c75cae57fcaf07150369a5f0b

--- frontend/src/app/map/page.js
+++ frontend/src/app/map/page.js
@@ -1,11 +1,36 @@
+'use client';
+
 import Image from "next/image";
 import MapAll from "public/assets/image/map-all.png";
-import styles from "./page.module.css"
+import styles from "./page.module.css";
+
+import * as THREE from 'three';
+import { Canvas, useFrame } from "@react-three/fiber";
+import { Suspense, useEffect, useRef } from "react";
+import { OrbitControls, Environment, Clone } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
+
+const Models = [
+    { name: "earth", url: "/assets/models/earth.glb" },
+];
+
+const Model = ({ url }) => {
+    const { scene } = useGLTF(url);
+    return <Clone object={scene} />;
+};
 
 export default function map() {
     return (
         <div className={styles.container}>
-            <Image className={styles.map_all} src={MapAll} alt="지도 전체 화면" />
+            <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
+                <Environment preset="city" />
+                <Suspense fallback={null}>
+                    <Model url={Models[0].url} />
+                    <ambientLight intensity={0.5} />
+                    <pointLight position={[10, 10, 10]} />
+                </Suspense>
+                <OrbitControls />
+            </Canvas>
         </div>
-    )
+    );
 }
