933312f76e680696091d74a4dcc32b95c6a1f486
이하린 <gmlwls608@gmail.com>
2023-10-31 17:20:39 +0900

    Feat: 지구 초기 화면 생성
    Change-Id: I4bfad395cf4e303c2a6626071e994da20e32ffcd

--- frontend/src/app/map/page.js
+++ frontend/src/app/map/page.js
@@ -1,36 +1,27 @@
 'use client';
 
-import Image from "next/image";
-import MapAll from "public/assets/image/map-all.png";
+import { useEffect, useRef } from "react";
 import styles from "./page.module.css";
 
-import * as THREE from 'three';
-import { Canvas, useFrame } from "@react-three/fiber";
-import { Suspense, useEffect, useRef } from "react";
-import { OrbitControls, Environment, Clone } from "@react-three/drei";
-import { useGLTF } from "@react-three/drei";
+import Globe from 'globe.gl';
 
-const Models = [
-    { name: "earth", url: "/assets/models/earth.glb" },
-];
 
-const Model = ({ url }) => {
-    const { scene } = useGLTF(url);
-    return <Clone object={scene} />;
-};
 
 export default function map() {
+
+    const globalRef = useRef(null);
+
+    useEffect(() => {
+        const globe = Globe()(globalRef.current);
+        globe.globeImageUrl('assets/image/earth.jpg')
+        return () => {
+            globe;
+        };
+    }, []);
+
     return (
         <div className={styles.container}>
-            <Canvas camera={{ position: [0, 0, -0.2], near: 0.025 }}>
-                <Environment preset="city" />
-                <Suspense fallback={null}>
-                    <Model url={Models[0].url} />
-                    <ambientLight intensity={0.5} />
-                    <pointLight position={[10, 10, 10]} />
-                </Suspense>
-                <OrbitControls />
-            </Canvas>
+            <div ref={globalRef} style={{ width: '100%', height: '500px' }}></div>
         </div>
     );
 }
