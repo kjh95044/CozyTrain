c7f78f3fa9290ee17ac9906c68cf8929e2ac391e
이하린 <gmlwls608@gmail.com>
2023-11-01 01:13:17 +0900

    Feat: 지도에 대륙 별 위치 라벨 찍기
    Change-Id: Ie91805f2fb070070e66dfff59a0ac7f3c49fb8ae

--- frontend/src/app/map/page.js
+++ frontend/src/app/map/page.js
@@ -7,12 +7,51 @@ import Globe from 'globe.gl';
 
 export default function map() {
 
+    const continentArray = [
+        {
+            lat: 42.13407,
+            lng: 120,
+            label: "아시아"
+        },
+        {
+            lat: 50.13407,
+            lng: 43.62981,
+            label: "유럽"
+        },
+        {
+            lat: 7.13407,
+            lng: 21.62981,
+            label: "아프리카"
+        },
+        {
+            lat: -27,
+            lng: 135,
+            label: "오세아니아"
+        },
+        {
+            lat: -18,
+            lng: -55,
+            label: "남아메리카"
+        },
+        {
+            lat: 38.13407,
+            lng: -100,
+            label: "북아메리카"
+        },
+    ];
+
     const globalRef = useRef(null);
 
     useEffect(() => {
         const globe = Globe()(globalRef.current);
         globe.globeImageUrl('assets/image/earth.jpg')
-        globe.backgroundImageUrl('assets/image/space.png')
+            .backgroundImageUrl('//unpkg.com/three-globe/example/img/night-sky.png')
+            .labelText('label')
+            .labelSize(2)
+            .labelDotRadius(2)
+            .labelColor(() => 'rgba(220,86,95,1)')
+            .labelsData(continentArray, { lat: 'lat', lng: 'lng', label: 'label' });
+
         return () => {
             globe;
         };
