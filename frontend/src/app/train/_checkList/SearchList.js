115438d93b36e8bc0435cbad8517ddf4f4dcaaed
이동주 <leednj0113@naver.com>
2023-11-02 16:46:37 +0900

    Feat: 상태관리 액서스토큰 추가
    Change-Id: I681ad8de92bd1cf11d5164dab75f3cee3776b9b3

--- frontend/src/app/train/_checkList/SearchList.js
+++ frontend/src/app/train/_checkList/SearchList.js
@@ -1,11 +1,25 @@
+import { useState } from "react";
+
 import styles from "./SearchList.module.css";
 
 export default function SearchList() {
+  const [value, setValue] = useState("");
+
+  const searchDrink = () => {
+    console.log(value);
+  };
+
   return (
     <>
       <div className={styles.searchBar}>
-        <input className={styles.input} type="text" />
-        <button className={styles.btn}>검색</button>
+        <input
+          className={styles.input}
+          type="text"
+          onChange={(e) => setValue(e.target.value)}
+        />
+        <button className={styles.btn} onClick={searchDrink}>
+          검색
+        </button>
       </div>
       <div className={styles.items}></div>
     </>
