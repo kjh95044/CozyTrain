963c8901177b437222db2bf8d7a894ac3d00f93a
이동주 <leednj0113@naver.com>
2023-11-02 14:36:57 +0900

    feat: 꿈 스웨거 등록
    Change-Id: I67e6911d340a8d4941dc66fbc2d181045b68f272

--- backend/cozytrain/src/main/java/com/ssafy/cozytrain/api/controller/DreamController.java
+++ backend/cozytrain/src/main/java/com/ssafy/cozytrain/api/controller/DreamController.java
@@ -4,6 +4,7 @@ import com.ssafy.cozytrain.api.dto.DreamDto;
 import com.ssafy.cozytrain.api.dto.DreamDto.DreamDtoReq;
 import com.ssafy.cozytrain.api.service.DreamService;
 import com.ssafy.cozytrain.common.utils.ApiUtils;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -18,26 +19,31 @@ public class DreamController {
     private final DreamService dreamService;
 
     @GetMapping("/{dreamId}")
+    @Operation(summary = "꿈 조회")
     public ApiUtils.ApiResult<DreamDto.DreamDtoRes> getDream(@PathVariable Long dreamId) {
         return success(dreamService.getDream(dreamId));
     }
 
     @GetMapping
+    @Operation(summary = "꿈 전체 조회")
     public ApiUtils.ApiResult<DreamDto.DreamDtoListRes> getDreams() {
         return success(dreamService.getDreams());
     }
 
     @PostMapping
+    @Operation(summary = "꿈 등록")
     public ApiUtils.ApiResult<Boolean> createDream(@RequestBody @Valid DreamDtoReq dreamDtoReq) {
         return success(dreamService.saveDream(dreamDtoReq));
     }
 
     @PatchMapping("/{dreamId}")
+    @Operation(summary = "꿈 수정")
     public ApiUtils.ApiResult<Boolean> updateDream(@PathVariable Long dreamId, @RequestBody @Valid DreamDtoReq dreamDtoReq) {
         return success(dreamService.updateDream(dreamId, dreamDtoReq));
     }
     
     @DeleteMapping("/{dreamId}")
+    @Operation(summary = "꿈 삭제")
     public ApiUtils.ApiResult<Boolean> deleteDream(@PathVariable Long dreamId) {
         return success(dreamService.deleteDream(dreamId));
     }
