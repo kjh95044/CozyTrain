3af759b32bb5d3d849d7c49e63aab3c12ded5b9d
이동주 <leednj0113@naver.com>
2023-11-02 09:34:00 +0900

    Chore: 미들웨어 일시 중단
    Change-Id: Ic9b2f4cc155a39393288d9338a3ea930d5be7677

--- frontend/src/middleware.js
+++ frontend/src/middleware.js
@@ -1,7 +1,12 @@
 import { NextResponse } from "next/server";
 
 export default function Middleware(request) {
-  console.log(request.cookies.getAll());
+  // if (!request.cookies.has("accessToken")) {
+  //   return NextResponse.redirect("http://localhost:3000/login");
+  // }
+  // if (request.cookies.get("todayFirstLogin").value === "true") {
+  //   return NextResponse.redirect("http://localhost:3000/train");
+  // }
 }
 
 // 미들웨어가 실행될 URL설정
