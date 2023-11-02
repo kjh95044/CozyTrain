b7997796d4ae85b7c3f9c76f71a6a79931a7ca0e
이하린 <gmlwls608@gmail.com>
2023-11-02 15:49:37 +0900

    Init: 한국 기찻길 초기 화면 생성
    Change-Id: I590cf6db1b6c11597fad16b7203aeea135a8fd5e

---
+++ frontend/src/app/map/korea/page.js
@@ -0,0 +1,33 @@
+'use client'
+
+import { Canvas } from "@react-three/fiber";
+import { Suspense } from "react";
+import { OrbitControls, Clone } from "@react-three/drei";
+import { useGLTF } from "@react-three/drei";
+
+import styles from "./page.module.css"
+
+
+const Models = [
+    { name: "ground", url: "/models/korea-ground.glb" }
+]
+
+const Model = ({ url, scale }) => {
+    const { scene } = useGLTF(url);
+    scene.scale.set(scale, scale, scale);
+    return <Clone object={scene} />;
+}
+
+export default function Korea() {
+    return (
+        <div className={styles.container}>
+            <Canvas camera={{ position: [0, 0.1, -0.2], near: 0.05 }}>
+                <Suspense fallback={null}>
+                    <Model url={Models[0].url} scale={0.16} />
+                    <ambientLight intensity={3} />
+                </Suspense>
+                <OrbitControls />
+            </Canvas>
+        </div >
+    )
+}
