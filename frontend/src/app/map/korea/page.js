ce43c281463d8ad9e457098c4b85d0384657f681
이하린 <gmlwls608@gmail.com>
2023-11-03 14:39:16 +0900

    Feat: 기차 3D 모델 화면에 띄우기
    Change-Id: I793b593a6c30faa829600d476fd1feeac3e1a7c1

--- frontend/src/app/map/korea/page.js
+++ frontend/src/app/map/korea/page.js
@@ -1,9 +1,11 @@
 'use client'
 
 import { Canvas } from "@react-three/fiber";
-import { Suspense } from "react";
+import { Suspense, useRef } from "react";
 import { OrbitControls, Clone } from "@react-three/drei";
 import { useGLTF } from "@react-three/drei";
+
+import * as THREE from 'three';
 
 import styles from "./page.module.css"
 import MapCloseButton from "../component/MapCloseButton";
@@ -12,16 +14,22 @@ import GlobeButton from "../component/GlobeButton";
 
 
 const Models = [
-    { name: "ground", url: "/models/korea-ground.glb" }
+    { name: "ground", url: "/models/korea-ground.glb", position: [0, 0, 0] },
+    { name: "train", url: "/models/red-train.glb", position: [-0.133, -0.003, -0.017] }
 ]
 
-const Model = ({ url, scale }) => {
+const Model = ({ url, scale, position }) => {
     const { scene } = useGLTF(url);
     scene.scale.set(scale, scale, scale);
+    scene.position.set(position[0], position[1], position[2])
     return <Clone object={scene} />;
 }
 
 export default function Korea() {
+
+    const group = useRef();
+    const groundRef = useRef();
+
     return (
         <div className={styles.container}>
             <GlobeButton />
@@ -29,15 +37,21 @@ export default function Korea() {
             <div className={styles.mapAllButton}>
                 <MapAllButton>대한민국</MapAllButton>
             </div>
-            <Canvas camera={{ position: [0, 0.06, -0.2], near: 0.038 }}>
+            <Canvas camera={{ position: [0, 0.03, -0.2], near: 0.038 }}>
                 <Suspense fallback={null}>
-                    <Model url={Models[0].url} scale={0.18} />
+                    <group ref={group}>
+                        <Model url={Models[0].url} scale={0.175} position={[Models[0].position[0], Models[0].position[1], Models[0].position[2]]} />
+
+                        <Model url={Models[1].url} scale={0.0003} position={[Models[1].position[0], Models[1].position[1], Models[1].position[2]]} />
+                    </group>
                     <ambientLight intensity={3} />
                 </Suspense>
                 <OrbitControls
                     enableRotate={true}
-                    enableZoom={false}
-                    maxPolarAngle={Math.PI / 2.5} />
+                    // enableZoom={false}
+                    // maxPolarAngle={Math.PI / 2.21}
+                    target={groundRef.current?.position || [0, 0, 0]}
+                />
             </Canvas>
         </div >
     )
