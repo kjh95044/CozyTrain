318102c2ddd903f94fd4d80ec330343f695a6311
강다은 <da010228@naver.com>
2023-11-03 16:07:26 +0900

    Feat: 날짜별 꿈 상세 조회
    Change-Id: Id0c3eb5e2c348abdc137038914de7386b3f0fe6f

--- frontend/src/app/Dream/[id]/page.js
+++ frontend/src/app/Dream/[id]/page.js
@@ -1,5 +1,53 @@
-"use-client";
+"use client";
 
-export default function Read(props) {
-  return <>{props.params.id}</>;
+import styles from "./page.module.css";
+import NavBottom from "@/components/NavBottom";
+import BackButton from "@/components/button/BackButton";
+import CancelButton from "@/components/button/CancelButton";
+import getFetch from "@/services/getFetch";
+import { useParams } from "next/navigation";
+import { useEffect, useState } from "react";
+
+export default function Detail() {
+  let c = useParams();
+  const [dream, setDream] = useState();
+
+  const getDream = async () => {
+    const data = await getFetch(`dream/${c.id}`);
+    console.log(data);
+    setDream(data.response);
+  };
+
+  useEffect(() => {
+    getDream();
+  }, [c]);
+
+  return (
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <b>꿈</b>
+      </div>
+
+      {dream ? (
+        <div className={styles.note_container}>
+          <div className={styles.title}>{dream.dreamDate}</div>
+          <textarea
+            name="content"
+            className={styles.content}
+            value={dream.dreamContent}
+            readOnly
+          />
+          <div className={styles.write_btn}>
+            <div>
+              <BackButton>뒤로가기</BackButton>
+            </div>
+          </div>
+        </div>
+      ) : (
+        ""
+      )}
+
+      <NavBottom />
+    </div>
+  );
 }
--- frontend/src/app/Dream/page.js
+++ frontend/src/app/Dream/page.js
@@ -1,8 +1,8 @@
 "use client";
 
 import "react-calendar/dist/Calendar.css";
-import styles from "./page.module.css";
 import "./page.css";
+import styles from "./page.module.css";
 import NavBottom from "@/components/NavBottom";
 import Link from "next/link";
 import getFetch from "@/services/getFetch";
@@ -10,25 +10,26 @@ import { useEffect, useState } from "react";
 import moment from "moment";
 import Image from "next/image";
 import Calendar from "react-calendar";
-import PrimaryButton from "@/components/button/PrimaryButton";
 
 export default function Dream() {
+  const [dream, setDream] = useState([]);
   const [dreams, setDreams] = useState([]);
-
-  const getDreams = async () => {
-    const data = await getFetch("dream");
-    setDreams(data.response.dreamDtoResList);
-  };
-
-  useEffect(() => {
-    getDreams();
-  }, []);
 
   // 캘린더 관련
   const [today, setToday] = useState(new Date()); // 현재 날짜
   const [nowDate, onChange] = useState(today); //선택한 날짜
   const monthOfActiveDate = moment(nowDate).format("YYYY-MM"); //보여지는 연도=월
   const [nowMonth, setNowMonth] = useState(monthOfActiveDate); //보여지는 달
+
+  const getDreams = async () => {
+    const data = await getFetch("dream");
+    setDreams(data.response.dreamDtoResList);
+    console.log(data.response.dreamDtoResList);
+  };
+
+  useEffect(() => {
+    getDreams();
+  }, []);
 
   const getNowMonth = (day) => {
     const newMonth = moment(day).format("YYYY-MM");
@@ -38,6 +39,14 @@ export default function Dream() {
   const isDateAfterToday = (date) => {
     return date > today;
   };
+
+  // 날짜 선택 시 꿈 있으면 보여주기
+  useEffect(() => {
+    const now = moment(nowDate).format("YYYY-MM-DD");
+    setDream(
+      dreams.find((day) => day.dreamDate === moment(now).format("YYYY-MM-DD"))
+    );
+  }, [dreams, nowDate]);
 
   // 날짜에 이모지 추가
   const addContent = ({ date }) => {
@@ -85,13 +94,31 @@ export default function Dream() {
           }}
           tileContent={addContent} //타일 안에 보여줄 컨텐츠
         />
+        <div className={styles.preview}>
+          {dream ? (
+            <div>
+              <Link href={`/dream/${dream.dreamId}`}>
+                <div className={styles.next_txt}>{dream.dreamDate}일의 꿈</div>
+              </Link>
+            </div>
+          ) : (
+            <div>
+              {nowDate.getDate() === today.getDate() ? (
+                <div>
+                  <Link href={`/dream/new`}>
+                    <div className={styles.next_txt}>
+                      오늘 꾼 꿈 적으러 가기
+                    </div>
+                  </Link>
+                </div>
+              ) : (
+                <p>꿈을 꾸지 않은 날입니다💤</p>
+              )}
+            </div>
+          )}
+        </div>
       </div>
-      <div className={styles.next_btn}>
-        {/* 오늘 꿈 있으면 안 보이게..? */}
-        <Link href={`/dream/new`}>
-          <div className={styles.next_txt}>오늘 꾼 꿈 적으러 가기</div>
-        </Link>
-      </div>
+
       <NavBottom />
     </div>
   );
