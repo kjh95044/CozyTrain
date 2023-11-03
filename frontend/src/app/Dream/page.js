535317df66a7a715c9d1ea8a105d0c72cf6e599c
강다은 <da010228@naver.com>
2023-11-03 10:54:50 +0900

    Feat: 꿈 목록 가져오기
    Change-Id: I2115d96553322a7d2056855d3f2089be29178c3d

--- frontend/src/app/Dream/page.js
+++ frontend/src/app/Dream/page.js
@@ -4,8 +4,22 @@ import styles from "./page.module.css";
 import NavBottom from "@/components/NavBottom";
 import PrimaryButton from "@/components/button/PrimaryButton";
 import Link from "next/link";
+import getFetch from "@/services/getFetch";
+import { useEffect, useState } from "react";
 
 export default function Dream() {
+  const [dreams, setDreams] = useState();
+
+  const getDreams = async () => {
+    console.log("getDreams");
+    const data = await getFetch("dream");
+    setDreams(data.response.dreamDtoResList);
+  };
+
+  useEffect(() => {
+    getDreams();
+  }, []);
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -13,8 +27,9 @@ export default function Dream() {
       </div>
       <div className={styles.note_container}>
         <div className={styles.next}>
+          {/* 오늘 꿈 있으면 안 보이게..? */}
           <Link href={`/dream/new`}>
-            <PrimaryButton>오늘의 꿈</PrimaryButton>
+            <PrimaryButton>오늘의 꾼 꿈 적으러 가기</PrimaryButton>
           </Link>
         </div>
       </div>
