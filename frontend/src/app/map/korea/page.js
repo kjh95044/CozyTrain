c1bcccf746073150e3d21d4dad5e10c27ee80c1d
이하린 <gmlwls608@gmail.com>
2023-11-03 17:49:31 +0900

    Feat: 기차 3D 위치, 회전 설정
    Change-Id: I2e32803554a437248f0527a77dcde5b4b5f7ecaa

--- frontend/src/app/map/korea/page.js
+++ frontend/src/app/map/korea/page.js
@@ -1,7 +1,7 @@
 'use client'
 
 import { Canvas } from "@react-three/fiber";
-import { Suspense, useRef } from "react";
+import { Suspense, useRef, useEffect } from "react";
 import { OrbitControls, Clone } from "@react-three/drei";
 import { useGLTF } from "@react-three/drei";
 
@@ -14,21 +14,27 @@ import GlobeButton from "../component/GlobeButton";
 
 
 const Models = [
-    { name: "ground", url: "/models/korea-ground.glb", position: [0, 0, 0] },
-    { name: "train", url: "/models/red-train.glb", position: [-0.133, -0.003, -0.017] }
+    { name: "ground", url: "/models/korea-ground.glb", position: [0, 0, 0], rotation: [0, 0, 0] },
+    { name: "train", url: "/models/red-train.glb", position: [-0.133, -0.003, -0.017], rotation: [0, 0, 0] }
 ]
 
-const Model = ({ url, scale, position }) => {
+const Model = ({ url, scale, position, rotation }) => {
     const { scene } = useGLTF(url);
     scene.scale.set(scale, scale, scale);
     scene.position.set(position[0], position[1], position[2])
+    scene.rotation.set(rotation[0], rotation[1], rotation[2])
     return <Clone object={scene} />;
 }
 
 export default function Korea() {
 
     const group = useRef();
-    const groundRef = useRef();
+
+    useEffect(() => {
+        if (group.current) {
+            controls.current.target.copy(group.current.position);
+        }
+    }, []);
 
     return (
         <div className={styles.container}>
@@ -37,20 +43,28 @@ export default function Korea() {
             <div className={styles.mapAllButton}>
                 <MapAllButton>대한민국</MapAllButton>
             </div>
-            <Canvas camera={{ position: [0, 0.03, -0.2], near: 0.038 }}>
+            <Canvas camera={{ position: [0, 0.03, -0.2], near: 0.038, lookAt: [-0.133, -0.003, -0.017] }}>
                 <Suspense fallback={null}>
+                    <Model
+                        url={Models[0].url}
+                        scale={0.175}
+                        position={[Models[0].position[0], Models[0].position[1], Models[0].position[2]]}
+                        rotation={[Models[0].rotation[0], Models[0].rotation[1], Models[0].rotation[2]]}
+                    />
                     <group ref={group}>
-                        <Model url={Models[0].url} scale={0.175} position={[Models[0].position[0], Models[0].position[1], Models[0].position[2]]} />
-
-                        <Model url={Models[1].url} scale={0.0003} position={[Models[1].position[0], Models[1].position[1], Models[1].position[2]]} />
+                        <Model
+                            url={Models[1].url}
+                            scale={0.0003}
+                            position={[Models[1].position[0], Models[1].position[1], Models[1].position[2]]}
+                            rotation={[Models[1].rotation[0], Models[1].rotation[1], Models[1].rotation[2]]} />
                     </group>
                     <ambientLight intensity={3} />
                 </Suspense>
                 <OrbitControls
                     enableRotate={true}
-                    // enableZoom={false}
-                    // maxPolarAngle={Math.PI / 2.21}
-                    target={groundRef.current?.position || [0, 0, 0]}
+                    enableZoom={false}
+                    maxPolarAngle={Math.PI / 2.21}
+                // target={group.current?.position || [0, 0, 0]}
                 />
             </Canvas>
         </div >
