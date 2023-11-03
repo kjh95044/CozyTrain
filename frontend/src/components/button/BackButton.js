9876ac67bbfbe85564d49b9f3223280d912fe9ea
강다은 <da010228@naver.com>
2023-11-03 16:06:50 +0900

    Design: content 크기 수정
    Change-Id: Iaa3c4739f4bcac1d3ebbd3df3f770401c2311541

--- frontend/src/components/button/BackButton.js
+++ frontend/src/components/button/BackButton.js
@@ -2,15 +2,16 @@
 import { useRouter } from "next/navigation";
 import styles from "./PrimaryButton.module.css";
 
-export default function PrimaryButton(props) {
+export default function BackButton(props) {
   const router = useRouter();
 
   const handleRedirect = () => {
     router.back();
   };
+
   return (
     <button className={styles.button} onClick={handleRedirect}>
-      {props.childrepn}
+      {props.children}
     </button>
   );
 }
