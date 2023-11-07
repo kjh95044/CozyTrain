cc5d45998ab1b64936f36e3a3fd42f42aee1cf75
이동주 <leednj0113@naver.com>
2023-11-07 15:37:52 +0900

    Feat: 체크리스트 구현
    Change-Id: Ie65260039a3f4b5cd379aa34aabb4ffbcd187477

--- frontend/src/app/train/_checkList/_today/page.js
+++ frontend/src/app/train/_checkList/_today/page.js
@@ -1,3 +1,63 @@
+import { useState, useEffect } from "react";
+
+import findDrinkById from "../findDrinkById";
+import PrimaryButton from "@/components/button/PrimaryButton";
+import deleteFetch from "@/services/deleteFetch";
+import getFetch from "@/services/getFetch";
+import styles from "./page.module.css";
+
 export default function Page() {
-  return;
+  const [checkList, setCheckList] = useState([]);
+
+  const getCheckList = async () => {
+    const resp = await getFetch("check-list");
+
+    if (!resp.response) {
+      setCheckList([]);
+      return;
+    }
+
+    const data = resp.response.checkListDtoList;
+    const promises = data.map(async (e) => {
+      const drinkName = await findDrinkById(e.elsId);
+      return { ...e, drinkName };
+    });
+
+    const newArr = await Promise.all(promises);
+    setCheckList(newArr);
+  };
+
+  const removeCheckListItem = async (checkListId) => {
+    await deleteFetch(`check-list/${checkListId}`);
+    getCheckList();
+  };
+
+  useEffect(() => {
+    getCheckList();
+  }, []);
+
+  return (
+    <>
+      <div className={styles.title}>
+        오늘 섭취한 <b>음료</b>입니다.
+      </div>
+      <div className={styles.itemContainer}>
+        {!checkList.length && <div>검색에서 음료를 추가해주세요.</div>}
+        {checkList.map((e) => {
+          return (
+            <div key={e.checkListId} className={styles.item}>
+              <div>{e.drinkName}</div>
+              <div
+                onClick={() => {
+                  removeCheckListItem(e.checkListId);
+                }}
+              >
+                <PrimaryButton>삭제</PrimaryButton>
+              </div>
+            </div>
+          );
+        })}
+      </div>
+    </>
+  );
 }
--- frontend/src/components/button/PrimaryButton.js
+++ frontend/src/components/button/PrimaryButton.js
@@ -1,5 +1,9 @@
 import styles from "./PrimaryButton.module.css";
 
 export default function PrimaryButton(props) {
-  return <button className={styles.button}>{props.children}</button>;
+  return (
+    <button className={styles.button} onClick={props.onClick}>
+      {props.children}
+    </button>
+  );
 }
