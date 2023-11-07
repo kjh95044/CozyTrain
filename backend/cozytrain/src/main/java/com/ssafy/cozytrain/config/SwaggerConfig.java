49314deb2290dd8edc68779bd566590f13ad2354
김지홍 <kjh95044@naver.com>
2023-11-07 15:54:45 +0900

    Chore: swagger Authorization 설정 추가

--- backend/cozytrain/src/main/java/com/ssafy/cozytrain/config/SwaggerConfig.java
+++ backend/cozytrain/src/main/java/com/ssafy/cozytrain/config/SwaggerConfig.java
@@ -5,13 +5,14 @@ import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
-import springfox.documentation.service.ApiInfo;
-import springfox.documentation.service.Server;
+import springfox.documentation.service.*;
 import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spi.service.contexts.SecurityContext;
 import springfox.documentation.spring.web.plugins.Docket;
 
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.List;
 
 @Configuration
 public class SwaggerConfig {
@@ -22,6 +23,8 @@ public class SwaggerConfig {
 
         return new Docket(DocumentationType.OAS_30)
                 .servers(serverLocal,devServer)
+                .securityContexts(List.of(this.securityContext()))
+                .securitySchemes(List.of(this.apiKey()))
                 .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.basePackage("com.ssafy.cozytrain"))
@@ -34,6 +37,24 @@ public class SwaggerConfig {
                 .title("CozyTrain API")
                 .version("1.0")
                 .build();
+    }
+
+    // JWT SecurityContext 구성
+    private SecurityContext securityContext() {
+        return SecurityContext.builder()
+                .securityReferences(defaultAuth())
+                .build();
+    }
+
+    private List<SecurityReference> defaultAuth() {
+        AuthorizationScope authorizationScope = new AuthorizationScope("global", "accessEverything");
+        AuthorizationScope[] authorizationScopes = new AuthorizationScope[1];
+        authorizationScopes[0] = authorizationScope;
+        return List.of(new SecurityReference("Authorization", authorizationScopes));
+    }
+
+    private ApiKey apiKey() {
+        return new ApiKey("Authorization", "Authorization", "header");
     }
 
 }
