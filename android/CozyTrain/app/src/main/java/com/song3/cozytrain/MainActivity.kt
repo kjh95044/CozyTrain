8b7c3376c1271bb1b917dbdb2475e2dd7647cfb9
김지홍 <kjh95044@naver.com>
2023-11-09 15:20:40 +0900

    Chore: 안드로이드 Init

---
+++ android/CozyTrain/app/src/main/java/com/song3/cozytrain/MainActivity.kt
@@ -0,0 +1,45 @@
+package com.song3.cozytrain
+
+import android.annotation.SuppressLint
+import android.net.http.SslError
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.webkit.SslErrorHandler
+import android.webkit.WebChromeClient
+import android.webkit.WebView
+import android.webkit.WebViewClient
+import androidx.appcompat.app.AlertDialog
+
+class MainActivity : AppCompatActivity() {
+    @SuppressLint("SetJavaScriptEnabled")
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val webView: WebView = findViewById(R.id.webView)
+
+        webView.settings.run {
+            // 웹뷰 자바스크립트 허용
+            javaScriptEnabled = true
+            javaScriptCanOpenWindowsAutomatically = true
+            setSupportMultipleWindows(true)
+        }
+
+        webView.webViewClient = SslWebViewConnect()
+        webView.webChromeClient = WebChromeClient()
+        webView.loadUrl(resources.getString(R.string.cozy_train_url))
+    }
+
+    override fun onBackPressed() { // 뒤로가기 기능 구현
+
+        val myWebView: WebView = findViewById(R.id.webView)
+        if (myWebView.canGoBack()) {
+            myWebView.goBack()
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+
+}
