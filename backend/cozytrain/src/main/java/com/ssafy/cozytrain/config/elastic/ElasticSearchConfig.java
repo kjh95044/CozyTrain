c24ce13f703746fe715e79bfa0e2658768a27e93
정효인 <hyoin.jeong3@gmail.com>
2023-11-10 10:32:51 +0900

    Chore: elastic search 설정 파일 수정

--- backend/cozytrain/src/main/java/com/ssafy/cozytrain/config/elastic/ElasticSearchConfig.java
+++ backend/cozytrain/src/main/java/com/ssafy/cozytrain/config/elastic/ElasticSearchConfig.java
@@ -1,6 +1,7 @@
 package com.ssafy.cozytrain.config.elastic;
 
 import org.elasticsearch.client.RestHighLevelClient;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.RestClients;
@@ -10,10 +11,17 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories // elasticsearch repository 허용
 public class ElasticSearchConfig extends AbstractElasticsearchConfiguration {
 
+    @Value("${spring.elasticsearch.username}")
+    private String userName;
+
+    @Value("${spring.elasticsearch.password}")
+    private String userPassword;
+
     @Override
     public RestHighLevelClient elasticsearchClient() {
         ClientConfiguration clientConfiguration = ClientConfiguration.builder()
                 .connectedTo("k9e104a.p.ssafy.io:9200")
+                .withBasicAuth(userName, userPassword)
                 .build();
         return RestClients.create(clientConfiguration).rest();
     }
