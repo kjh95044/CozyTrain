d7a0bbe2b9606b1cdb2f798dc0abe18f01108eb7
강다은 <da010228@naver.com>
2023-11-14 02:19:28 +0900

    Feat: 지도 로딩 페이지
    Change-Id: I3e63902596e3527fd21a7a3b44a697e7d854d9d6

--- android/CozyTrain/app/src/main/java/song/sam/cozytrain/ui/screens/mydata/MyDataState.kt
+++
@@ -1,6 +0,0 @@
-package song.sam.cozytrain.ui.screens.mydata
-
-//sealed class MyDataState {
-//    object NoSelection : MyDataState()
-//    data class Selected(val index: Int) : MyDataState()
-//}
