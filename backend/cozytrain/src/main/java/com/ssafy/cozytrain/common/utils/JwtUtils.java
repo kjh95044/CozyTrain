9b8be72546f5151072c7cb5284fdb791fdca7e29
황주원 <woneee99@gmail.com>
2023-11-14 11:40:24 +0900

    Fix: token 기간 변경
    Change-Id: I642d44f68964600868ede20c47be32d183b13f89

--- backend/cozytrain/src/main/java/com/ssafy/cozytrain/common/utils/JwtUtils.java
+++ backend/cozytrain/src/main/java/com/ssafy/cozytrain/common/utils/JwtUtils.java
@@ -30,7 +30,7 @@ public class JwtUtils {
     private final RefreshTokenRepository refreshTokenRepository;
     private final MemberDetailServiceImpl memberDetailService;
 
-    private static final long ACCESS_TIME =  60 * 60 * 1000L;
+    private static final long ACCESS_TIME =  3 * 24 * 60 * 60 * 1000L;
     private static final long REFRESH_TIME = 14 * 24 * 60 * 60 * 1000L;
     public static final String ACCESS_TOKEN = "Access_Token";
     public static final String REFRESH_TOKEN = "Refresh_Token";
