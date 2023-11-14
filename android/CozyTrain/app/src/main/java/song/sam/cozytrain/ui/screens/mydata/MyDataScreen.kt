38aa1d91deb0b94160d2b15375a95f74db7e80e5
강다은 <da010228@naver.com>
2023-11-14 09:05:33 +0900

    Fix: 권한 허용 저장
    Change-Id: I4e38772d3549382f510304afd7f0f7b063bdfb7e

--- android/CozyTrain/app/src/main/java/song/sam/cozytrain/ui/screens/mydata/MyDataScreen.kt
+++ android/CozyTrain/app/src/main/java/song/sam/cozytrain/ui/screens/mydata/MyDataScreen.kt
@@ -1,5 +1,6 @@
 package song.sam.cozytrain.ui.screens.mydata
 
+import android.util.Log
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
@@ -14,22 +15,28 @@ import song.sam.cozytrain.ui.component.DrawHealthConnectSubscreen
 import song.sam.cozytrain.ui.component.ViewModelData
 import song.sam.cozytrain.ui.healthconnect.viewmodel.SleepSessionViewModel
 import song.sam.cozytrain.ui.healthconnect.viewmodel.StepsViewModel
+import song.sam.cozytrain.utils.UserDataViewModel
 
 @Destination
 @Composable
 fun MyDataScreen(
     sleepSessionViewModel: SleepSessionViewModel = hiltViewModel(),
     stepsViewModel: StepsViewModel = hiltViewModel(),
+    dataViewModel: UserDataViewModel = hiltViewModel()
 ) {
+    Log.d("dataViewModel","$dataViewModel")
     DrawMyDataScreen(
         sleepVMD = sleepSessionViewModel.getViewModelData(),
-        stepsVMD = stepsViewModel.getViewModelData())
+        stepsVMD = stepsViewModel.getViewModelData(),
+        dataViewModel = dataViewModel
+    )
 }
 
 @Composable
 private fun DrawMyDataScreen(
     sleepVMD: ViewModelData<SleepSessionData>,
     stepsVMD: ViewModelData<StepsRecord>,
+    dataViewModel: UserDataViewModel
 ) {
     Column(
         modifier = Modifier
@@ -41,6 +48,7 @@ private fun DrawMyDataScreen(
         DrawHealthConnectSubscreen(
             viewModelData1 = stepsVMD,
             viewModelData2 = sleepVMD,
+            userModelData = dataViewModel,
         )
     }
 
